bbd3804bf02ad41d863e4b66a8ef0847f4853cf4
lg <154286411@qq.com>
2021-02-02 19:53:19 +0800

    add test folder

---
+++ test/bcrypt.mjs
@@ -0,0 +1,11 @@
+import bcrypt from 'bcrypt-nodejs'
+
+//github:https://github.com/shaneGirish/bcrypt-nodejs
+
+
+bcrypt.genSalt(10,(error,result)=>{
+    bcrypt.hash("bacon", null, null, function(err, hash) {
+        // Store hash in your password DB.
+        console.log(hash)
+      });
+})
